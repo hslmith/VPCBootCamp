1da14d0e55fe2df987533a824884a633c994ddfb
hslmith <47323378+hslmith@users.noreply.github.com>
2022-04-08 08:47:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "ibm_is_security_group_rule" "public_facing_tcp22" {
     group = "${ibm_is_security_group.public_facing_sg.id}"
     direction = "inbound"
     remote = "0.0.0.0/0"
-    tcp = {
+    tcp {
       port_min = "22"
       port_max = "22"
     }
@@ -38,7 +38,7 @@ resource "ibm_is_security_group_rule" "public_facing_sg_tcp80" {
     group = "${ibm_is_security_group.public_facing_sg.id}"
     direction = "inbound"
     remote = "0.0.0.0/0"
-    tcp = {
+    tcp {
       port_min = "80"
       port_max = "80"
     }
@@ -48,7 +48,7 @@ resource "ibm_is_security_group_rule" "public_facing_icmp" {
     group = "${ibm_is_security_group.public_facing_sg.id}"
     direction = "inbound"
     remote = "0.0.0.0/0"
-    icmp = {
+    icmp {
       code = "0"
       type = "8"
     }
