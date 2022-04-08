582ebba628a4bd68359c327c3a469e824819e75f
hslmith <47323378+hslmith@users.noreply.github.com>
2022-04-08 08:36:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,56 @@ resource "ibm_is_vpc" "vpc1" {
   name = "${var.vpc_name}"
   address_prefix_management = "manual"
 }
+
+//--- security group creation for web tier
+
+
+resource "ibm_is_security_group" "public_facing_sg" {
+    name = "${var.vpc_name}-public-facing-sg1"
+    vpc  = "${ibm_is_vpc.vpc1.id}"
+}
+
+resource "ibm_is_security_group_rule" "public_facing_tcp22" {
+    group = "${ibm_is_security_group.public_facing_sg.id}"
+    direction = "inbound"
+    remote = "0.0.0.0/0"
+    tcp = {
+      port_min = "22"
+      port_max = "22"
+    }
+}
+
+resource "ibm_is_security_group_rule" "public_facing_sg_tcp80" {
+    group = "${ibm_is_security_group.public_facing_sg.id}"
+    direction = "inbound"
+    remote = "0.0.0.0/0"
+    tcp = {
+      port_min = "80"
+      port_max = "80"
+    }
+}
+
+resource "ibm_is_security_group_rule" "public_facing_icmp" {
+    group = "${ibm_is_security_group.public_facing_sg.id}"
+    direction = "inbound"
+    remote = "0.0.0.0/0"
+    icmp = {
+      code = "0"
+      type = "8"
+    }
+}
+
+resource "ibm_is_security_group_rule" "public_facing_egress" {
+    group = "${ibm_is_security_group.public_facing_sg.id}"
+    direction = "outbound"
+    remote = "0.0.0.0/0"
+}
+
+
+//--- security group creation for db tier
+
+resource "ibm_is_security_group" "private_facing_sg" {
+    name = "${var.vpc_name}-private-facing-sg"
+    vpc = "${ibm_is_vpc.vpc1.id}"
+}
+
