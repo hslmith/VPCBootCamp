69dcb51c00c7a4e54fdb4d49c5c6d1e02c18693b
hslmith <47323378+hslmith@users.noreply.github.com>
2022-04-08 08:54:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,3 +80,128 @@ resource "ibm_is_public_gateway" "pubgw-zone1" {
   vpc  = "${ibm_is_vpc.vpc1.id}"
   zone = "${var.zone1}"
 }
+
+
+
+/////////////////////
+//   ZONE 1 (L)
+/////////////////////
+
+//--- address prexix for VPC
+
+resource "ibm_is_vpc_address_prefix" "prefix_z1" {
+  name = "vpc-zone1-cidr"
+  zone = "${var.zone1}"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  cidr = "${var.zone1_prefix}"
+}
+
+//--- subnets for web and db tier
+
+resource "ibm_is_subnet" "websubnet1" {
+  name            = "web-subnet-zone1"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone1}"
+  ipv4_cidr_block = "${var.web_subnet_zone1}"
+  depends_on      = ["ibm_is_vpc_address_prefix.prefix_z1"]
+}
+
+
+resource "ibm_is_subnet" "dbsubnet1" {
+  name            = "db-subnet-zone1"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone1}"
+  ipv4_cidr_block = "${var.db_subnet_zone1}"
+  depends_on      = ["ibm_is_vpc_address_prefix.prefix_z1"]
+}
+
+
+//--- Web Server(s)
+
+resource "ibm_is_instance" "web-instancez01" {
+  count   = "${var.web_server_count}"
+  name    = "webz01-${count.index+1}"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet = "${ibm_is_subnet.websubnet1.id}"
+    security_groups = ["${ibm_is_security_group.public_facing_sg.id}"]
+  }
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone1}"
+  keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
+  user_data = "${data.local_file.cloud-config-web-txt.content}
+}
+
+
+
+//--- DB Server(s) 
+
+
+resource "ibm_is_instance" "db-instancez01" {
+  count   = "${var.db_server_count}"
+  name    = "dbz01-${count.index+1}"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet = "${ibm_is_subnet.dbsubnet1.id}"
+    security_groups = ["${ibm_is_security_group.private_facing_sg.id}"]
+  }
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone1}"
+  keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
+  //user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
+}
+
+
+##############################################################################
+# ZONE 2 (R)
+##############################################################################
+
+//--- address prexix for VPC
+
+resource "ibm_is_vpc_address_prefix" "prefix_z2" {
+  name = "vpc-zone2-cidr"
+  zone = "${var.zone2}"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  cidr = "${var.zone2_prefix}"
+}
+
+//--- subnets for web and db tier
+
+resource "ibm_is_subnet" "websubnet2" {
+  name            = "web-subnet-zone2"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone2}"
+  ipv4_cidr_block = "${var.web_subnet_zone2}"
+  depends_on      = ["ibm_is_vpc_address_prefix.prefix_z2"]
+}
+
+
+resource "ibm_is_subnet" "dbsubnet2" {
+  name            = "db-subnet-zone2"
+  vpc             = "${ibm_is_vpc.vpc1.id}"
+  zone            = "${var.zone2}"
+  ipv4_cidr_block = "${var.db_subnet_zone2}"
+  depends_on      = ["ibm_is_vpc_address_prefix.prefix_z2"]
+}
+
+//--- Web Server(s)
+
+resource "ibm_is_instance" "web-instancez02" {
+  count   = "${var.web_server_count}"
+  name    = "webz02-${count.index+1}"
+  image   = "${var.image}"
+  profile = "${var.profile}"
+
+  primary_network_interface = {
+    subnet = "${ibm_is_subnet.websubnet2.id}"
+    security_groups = ["${ibm_is_security_group.public_facing_sg.id}"]
+  }
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone2}"
+  keys = ["${data.ibm_is_ssh_key.sshkey1.id}"]
+  user_data = "${data.template_cloudinit_config.cloud-init-web.rendered}"
+}
