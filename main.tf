b34370d271aaddfb26567ed8f7682f27bbaa8ffe
hslmith <47323378+hslmith@users.noreply.github.com>
2022-04-08 08:45:29 -0500

    update

--- main.tf
+++ main.tf
@@ -68,3 +68,15 @@ resource "ibm_is_security_group" "private_facing_sg" {
     vpc = "${ibm_is_vpc.vpc1.id}"
 }
 
+
+
+///////////////////////////////////////
+// Public Gateway's for Zone 1
+////////////////////////////////////////
+
+
+resource "ibm_is_public_gateway" "pubgw-zone1" {
+  name = "${var.vpc_name}-${var.zone1}-pubgw"
+  vpc  = "${ibm_is_vpc.vpc1.id}"
+  zone = "${var.zone1}"
+}
--- variables.tf
+++ variables.tf
@@ -48,3 +48,65 @@ variable "zone2_prefix" {
   default = "10.20.0.0/18"
   description = "CIDR block to be used for zone 2"
 }
+
+////////////////////////////////
+// Define Subnets for zones
+////////////////////////////////
+
+variable "web_subnet_zone1" {
+  default = "10.10.10.0/24"
+}
+
+variable "db_subnet_zone1" {
+  default = "10.10.20.0/24"
+}
+
+
+
+variable "web_subnet_zone2" {
+  default = "10.20.30.0/24"
+}
+
+variable "db_subnet_zone2" {
+  default = "10.20.40.0/24"
+}
+
+////////////////////////////////
+
+
+
+
+variable "ssh_key_name" {
+  default = "vpc-rsa"
+  description = "Name of existing VPC SSH Key"
+}
+
+variable "web_server_count" {
+  default = 1
+}
+
+variable "db_server_count" {
+  default = 1
+}
+
+variable "image" {
+  default = "r006-b9ebf1d8-a674-42e8-898e-0fcb08994f66"
+  description = "OS Image ID to be used for virtual instances"
+}
+
+variable "profile" {
+  default = "bx2-2x8  "
+  description = "Instance profile to be used for virtual instances"
+}
+
+
+// LBaaS Define
+
+
+variable "webtier-lb-connections" {
+  default = 2000
+}
+
+variable "webtier-lb-algorithm" {
+  default = "round_robin"
+}
